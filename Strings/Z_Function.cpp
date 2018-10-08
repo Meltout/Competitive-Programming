5adf577029527425d69a1acb2c5114e91f4d002b
Meltout <31480682+Meltout@users.noreply.github.com>
2018-10-08 22:54:57 +0300

    Add files via upload

---
+++ Strings/Z_Function.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#define pb push_back
+#define mp make_pair
+using namespace std;
+
+vector <int> z_function(string &s)
+{
+    int n = s.size();
+    vector <int> z(n);
+
+    z[0] = n;
+    for(int i = 1, l = 0, r = 0; i < n; i++)
+    {
+        if(i <= r)
+          z[i] = min(r - i + 1, z[i - l]);
+        while(i + z[i] < n && s[z[i]] == s[i + z[i]])
+            z[i]++;
+        if(i + z[i] - 1 > r)
+        {
+            l = i;
+            r = i + z[i] - 1;
+        }
+    }
+
+    return z;
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+
+    return 0;
+}
